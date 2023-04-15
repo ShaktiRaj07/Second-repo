d97ea911c4b01d7656c711ab602c08c8b7b36adf
Shakti <shaktiraj.gour@dotsquares.com>
2023-04-15 10:44:41 +0530

    Sab sahi  hogya

---
+++ src/components/locationDetail/MyStore.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const Mystore = (props: any) => {
+
+    return (
+        <>
+            <div>
+                <p>
+                    {props.props.heading}
+                </p>
+                <p>{props.props.description}</p>
+                <img src={props.props.image.url} alt="acd" />
+                <button>{props.props.cTA.label}</button>
+            </div>
+        </>
+    )
+}
+
+export default Mystore;
---
+++ src/config/answersHeadlessConfig.ts
@@ -0,0 +1,20 @@
+
+export const AnswerExperienceConfig = {
+  limit:3,
+  locale: "en",
+  apiKey : "60681b352f68f9d27b208e35e48fb140",
+  verticalKey : "locations",
+  experienceKey : "pets-details",
+  experienceVersion: "STAGING",
+  locationRadius: 804672,
+  sessionTrackingEnabled: true,
+  endpoints: {
+    universalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+    verticalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+    questionSubmission: "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+    universalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+    verticalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+    filterSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch",
+
+  }
+}
